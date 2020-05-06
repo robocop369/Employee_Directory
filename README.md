7deea3c01e5eee3c63e49f0f6643ec916696f4f3
unknown <jcontreras39@gmail.com>
2020-05-05 22:27:18 -0500

    working directory

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# HW_19
+# Unit 19 React Homework: Employee Directory
+
+## Link to Deployed Application
+
+[Employee Directory](https://damp-cliffs-93023.herokuapp.com/)
+
+## Overview
+
+For this assignment, I created a simple employee directory with React. Upon load, it fakes an employee directory by importing a list of people from randomuser.me. Once on the page, the user is able to search by first name, last name, or email address to find the person they're looking for. The list of employees renders dynamically as you type, helping the user narrow down their search. The information about the employees provided to the user include a profile image (for when you need to match a face to the name!), phone number, email, and date of birth (to help plan those fun office parties!).
+
+![Bob](https://randomuser.me/api/portraits/med/men/74.jpg) ![Steve](https://randomuser.me/api/portraits/med/men/63.jpg) ![Carrie](https://randomuser.me/api/portraits/med/women/20.jpg)
+
+## User Story
+
+* As a user, I want to be able to view my entire employee directory at once so that I have quick access to their information.
+
+## Business Context
+
+An employee or manager would benefit greatly from being able to view non-sensitive data about other employees. It would be particularly helpful to be able to filter employees by name.
+
+## License 
+
+MIT License
+
+Copyright (c) 2020 Jody Russell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/components/Directory.js
@@ -0,0 +1,124 @@
+import React, { Component } from "react";
+import Employees from "./Employees.js";
+import Search from "./Search.js";
+import "../styles/Directory.css";
+
+class Directory extends Component {
+
+  state = {
+    employees: [],
+    empSort: [],
+    order: "descend",
+    search: "",
+    sorted: false,
+  };
+
+  // check that the component rendered at least once, and pull in our data
+  // wait for the information to come back
+  componentDidMount = () => {
+    fetch(`https://randomuser.me/api/?results=75&nat=us&inc=name,email,phone,id,picture,dob`)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ employees: json.results })
+      })
+  };
+
+  // sort through employees based on search term
+  // check if there is a match and set that to empSort for rendering
+  sortEmp = () => {
+    let { employees, search } = this.state;
+    let empSort = employees.filter(sorted => {
+      return (
+        sorted.name.first.toLowerCase().includes(search.toLowerCase()) ||
+        sorted.name.last.toLowerCase().includes(search.toLowerCase()) ||
+        sorted.email.toLowerCase().includes(search.toLowerCase())
+      )
+    })
+    this.setState({ empSort })
+  }
+
+  // grab search term, activate sorted  
+  startSort = event => {
+    this.setState({ search: event.target.value }, () => {
+      this.sortEmp();
+      this.setState({ sorted: true });
+    });
+  };
+
+  render = () => {
+
+    return (
+
+      <div>
+        <div className="jumbotron">
+          <h2 className="display-4">
+            Employee Directory
+          </h2>
+          <p >
+            Find your coworkers!
+          </p>
+          <p > 
+            Search below by name or email to filter through the directory
+          </p>
+          <Search
+            name="search"
+            startSort={this.startSort}
+            label="Search"
+          />
+        </div>
+
+        <div className="container">
+          <table className="table">
+            <thead className="thead">
+              <tr>
+                <th>Headshot  </th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                {/* <th>Birthdate  </th> */}
+              </tr>
+            </thead>
+            <tbody>
+
+              {!this.state.sorted ? this.state.employees.map(employee => (
+
+
+                < Employees
+                  key={employee.id.value}
+                  firstName={employee.name.first}
+                  lastName={employee.name.last}
+                  phone={employee.phone}
+                  email={employee.email}
+                  icon={employee.picture.medium}
+                  // dob={employee.dob.date}
+
+                />
+
+              ))
+                : this.state.empSort.map(employee => (
+
+                  <Employees
+                    key={employee.id.value}
+                    firstName={employee.name.first}
+                    lastName={employee.name.last}
+                    phone={employee.phone}
+                    email={employee.email}
+                    icon={employee.picture.medium}
+                    // dob={employee.dob.date}
+                  />
+
+                ))};
+          </tbody>
+          </table>
+
+        </div>
+
+      </div >
+
+    )
+
+  }
+
+}
+
+export default Directory;
